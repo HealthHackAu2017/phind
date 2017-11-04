d87deddb951d672f5e422b24e8382ebf581e5cab
Omkar <okpatil4u@gmail.com>
2017-11-04 20:52:53 +1000

    New Changes

--- client/scripts/controllers/diary.controller.js
+++ client/scripts/controllers/diary.controller.js
@@ -8,6 +8,55 @@ export default class DiaryCtrl extends Controller {
     this.helpers({
       data() {
         return Diaries.find();
+      },
+      color(){
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+
+
+      },  
+      words(){
+        return [
+            {id: 1, word: "oke", size: 1},
+            {id: 2, word: "blabla", size: 6},
+            {id: 3, word: "test", size: 7},
+            {id: 4, word: "schaap", size: 2},
+            {id: 5, word: "deployment", size: 10},
+            {id: 6, word: "woord3", size: 3},
+            {id: 7, word: "wogamalord4", size: 4},
+            {id: 8, word: "woord5", size: 5},
+            {id: 9, word: "woord8", size: 8},
+            {id: 10, word: "woord9", size: 9},
+            {id: 1, word: "oke", size: 1},
+            {id: 2, word: "blabla", size: 6},
+            {id: 3, word: "test", size: 7},
+            {id: 4, word: "schaap", size: 2},
+            {id: 5, word: "deployment", size: 10},
+            {id: 6, word: "woord3", size: 3},
+            {id: 7, word: "wogamalord4", size: 4},
+            {id: 8, word: "woord5", size: 5},
+            {id: 9, word: "woord8", size: 8},
+            {id: 10, word: "woord9", size: 9},
+            {id: 1, word: "oke", size: 1},
+            {id: 2, word: "blabla", size: 6},
+            {id: 3, word: "test", size: 7},
+            {id: 4, word: "schaap", size: 2},
+            {id: 5, word: "deployment", size: 10},
+            {id: 6, word: "woord3", size: 3},
+            {id: 7, word: "wogamalord4", size: 4},
+            {id: 8, word: "woord5", size: 5},
+            {id: 9, word: "woord8", size: 8},
+            {id: 10, word: "woord9", size: 9},
+            {id: 1, word: "oke", size: 1},
+            {id: 2, word: "blabla", size: 6},
+            {id: 3, word: "test", size: 7},
+            {id: 4, word: "schaap", size: 2},
+            {id: 5, word: "deployment", size: 10}
+        ];
       }
     });
   }
